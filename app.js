a95c29df3eca6cf16e695422d384d36fac35b271
lekejosh <lekejosh6wf@gmail.com>
2023-05-25 22:25:10 +0100

    Setting Up

---
+++ app.js
@@ -0,0 +1,48 @@
+/*
+ * app.js
+ * Created By Adeleke Joshua A.
+ * 25/05/2023
+ */
+
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const errorMiddleware = require("./middlewares/error");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret:
+      process.env.COOKIE_SECRET ||
+      "c613c888f2ffba9a939d8d9be116470fe765b238fd5b579be06dc235fa5bf23a",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      expires: Date.now() + 1000 + 60 * 60 * 24 * 7,
+      maxAge: 1000 + 60 * 60 * 24 * 7,
+    },
+  })
+);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: true,
+    message: "You probably shouldn't be here, but...",
+    data: {
+      service: "inspiraspace-api",
+      version: "1.0",
+    },
+  });
+});
+app.use(errorMiddleware);
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,44 @@
+/*
+ * server.js
+ * Created By Adeleke Joshua A.
+ * 25/05/2023
+ */
+const app = require("./app");
+const mongoose = require("mongoose");
+const cloudinary = require("cloudinary");
+require("dotenv").config();
+
+process.on("uncaughtException", (err) => {
+  console.log(`Error: $err: ${err.message}`);
+  console.log(`Shutting down the server due to uncaught Expectation`);
+  process.exit(1);
+});
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_APIKEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(`${process.env.DB_URI}/${process.env.DB_NAME}`)
+  .then(() =>
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is working on http://localhost:${process.env.PORT}`);
+    })
+  )
+  .catch((err) => {
+    console.error(err);
+  });
+
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: $err: ${err.message}`);
+  console.log(`Shutting down the server due to unhandled promise Rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
+    
